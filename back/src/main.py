6ed98a0268a22fb47bebc759895d42ad3fcf774a
ernest <ernest_s@bk.ru>
2026-04-30 13:25:09 +0330

    add lifespan

--- back/src/main.py
+++ back/src/main.py
@@ -2,17 +2,21 @@ from fastapi import FastAPI
 import uvicorn
 from db.session import engine, get_db
 from sqlalchemy import select
+from contextlib import asynccontextmanager
 from models import Base
 from routes.module import router as module_router
+from routes.answer import router as module_answer
 
-try:
-    Base.metadata.create_all(bind=engine)
-except:
-    ...
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-app = FastAPI(root_path="/api")
+app = FastAPI(root_path="/api", lifespan=lifespan)
 
 app.include_router(module_router)
+app.include_router(module_answer)
 
 @app.get("/")
 async def root():
